85086f326519590c482738abf2779cb25d4acb4c
otumuyen-gospel <otumsgosepel@gmail.com>
2025-11-30 04:22:25 +0100

    created capture method  entity

---
+++ apis/capturemethod/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class CaptureMethod(models.Model):
+    METHOD_FACE = 'FACE'
+    METHOD_FORM = 'FORM'
+    CAPTURE_METHODS = [
+        (METHOD_FACE,'FACE'),
+        (METHOD_FORM,'FORM'),
+    ]
+    method = models.CharField(choices=CAPTURE_METHODS,
+                               default=METHOD_FORM, unique=True)
+    
+    METHOD_DESCRIPTION_FACE = 'Face Recognition'
+    METHOD_DESCRIPTION_FORM = 'Manual Form Entry'
+    METHODS_DESCRIPTION = [
+        (METHOD_DESCRIPTION_FACE,'Face Recognition'),
+        (METHOD_DESCRIPTION_FORM,'Manual Form Entry'),
+    ]
+    description = models.CharField(choices=METHODS_DESCRIPTION,
+                               default=METHOD_DESCRIPTION_FORM, unique=True)
+    
+    class Meta:
+        ordering = ('method',)
+    def __str__(self):
+        return f"{self.description}"
+    
+    
