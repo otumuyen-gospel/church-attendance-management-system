a7d5d026a4f24c983ec203c624838688fad95589
otumuyen-gospel <otumsgosepel@gmail.com>
2025-12-02 11:05:26 +0100

    create ministries and services entitty

---
+++ apis/ministries/views.py
@@ -0,0 +1,64 @@
+from .models import Ministries
+from .serializers import MinistriesSerializers
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from urllib.parse import urlparse
+from rest_framework.permissions import IsAuthenticated, IsAdminUser,AllowAny
+from django_filters import AllValuesFilter, DateTimeFilter, NumberFilter
+from rest_framework.exceptions import PermissionDenied
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from django.contrib.auth.models import Group, Permission
+from role.util import requiredGroups
+
+
+#this generic class will handle GET method to be used by the admin alone
+class MinistriesList(generics.ListAPIView):
+    queryset = Ministries.objects.all()
+    serializer_class = MinistriesSerializers
+    permission_classes = [AllowAny,]
+    required_groups = requiredGroups(permission='view_ministries')
+    name = 'ministries-list'
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    
+    #you can filter by field names specified here keyword e.g url?name='church one'
+    filterset_fields = ('name','description','churchId__id')  
+
+     #you can search using the "search" keyword
+    search_fields = ('name','description','churchId__id')   
+
+    #you can order using the "ordering" keyword
+    ordering_fields = ('name','description','churchId__id') 
+
+
+class UpdateMinistries(generics.UpdateAPIView):
+    queryset = Ministries.objects.all()
+    serializer_class = MinistriesSerializers
+    permission_classes = [AllowAny,]
+    required_groups = requiredGroups(permission='change_ministries')
+    name = 'ministries-update'
+    lookup_field = "id"
+
+
+class DeleteMinistries(generics.DestroyAPIView):
+    queryset = Ministries.objects.all()
+    serializer_class = MinistriesSerializers
+    permission_classes = [AllowAny,]
+    required_groups = requiredGroups(permission='delete_ministries')
+    name = 'delete-ministries'
+    lookup_field = "id"
+
+
+class CreateMinistries(generics.CreateAPIView):
+    queryset = Ministries.objects.all()
+    serializer_class = MinistriesSerializers
+    permission_classes = [AllowAny,]
+    required_groups = requiredGroups(permission='add_ministries')
+    name = 'create-ministries'
+    
---
+++ apis/services/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from church.models import Church
+from ministries.models import Ministries
+
+# Create your models here.
+class Services(models.Model):
+    churchId = models.ForeignKey(Church, on_delete=models.CASCADE)
+    ministryId = models.ForeignKey(Ministries, on_delete=models.SET_NULL, null=True, blank=True)
+    eventName = models.TextField(blank=False)
+    eventDate = models.DateField(blank=True, null=True)
+    eventTime = models.TimeField(blank=False)
+    MONDAY = 'MON'
+    TUESDAY = 'TUE'
+    WEDNESDAY = 'WED'
+    THURSDAY = 'THU'
+    FRIDAY = 'FRI'
+    SATURDAY = 'SAT'
+    SUNDAY = 'SUN'
+    EVENT_DAY = [
+       (MONDAY, 'MON'),
+       (TUESDAY,'TUE'),
+       (WEDNESDAY, 'WED'),
+       (THURSDAY, 'THU'),
+       (FRIDAY, 'FRI'),
+       (SATURDAY, 'SAT'),
+       (SUNDAY,'SUN'),
+    ]
+    eventDay = models.CharField(choices=EVENT_DAY,
+                               default=SUNDAY,)
+    
+    location = models.TextField(blank=False)
+    isReoccuring = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ("eventName",)
+    def __str__(self):
+        return f'{self.eventName}'
