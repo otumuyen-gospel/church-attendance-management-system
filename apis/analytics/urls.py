8b5c328810c6da2a0623a625e34cc6e6bf3786ec
otumuyen-gospel <otumsgosepel@gmail.com>
2026-01-02 14:42:07 +0100

    building api for analytics

--- apis/analytics/urls.py
+++ apis/analytics/urls.py
@@ -1,3 +1,23 @@
-from django.urls import path
+"""
+URL configuration for backend project.
 
-urlpatterns = []
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('analytics/', Analytics.as_view(), name='analytics'),
+   
+]
