a815f9d65e87b1e79a2450bcfe99a24425f99663
otumuyen-gospel <otumsgosepel@gmail.com>
2026-01-07 15:37:14 +0100

    removed the followup apis

--- apis/apis/settings.py
+++ apis/apis/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'contact',
     'leadership',
     'attendance',
-    'followup',
     'faces',
     'user',
     'auth',
--- apis/followup/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import FollowUp
-
-
-class FollowUpSerializers(serializers.ModelSerializer):
-
-    class Meta:
-        model = FollowUp
-        fields = "__all__"
