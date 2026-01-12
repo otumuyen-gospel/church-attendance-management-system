bc53524259fd0bc08abc54e3601b6e537d67dec4
otumuyen-gospel <otumsgosepel@gmail.com>
2026-01-12 12:38:45 +0100

    finalizing the implementation of sms api

--- apis/message/urls.py
+++ apis/message/urls.py
@@ -19,8 +19,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('views/', MessageList.as_view(), name='message-list'),
-    path('send-sms/', CreateTextMessage.as_view(), name='text-message'),
+    path('send-sms/', SendSMS.as_view(), name='text-message'),
     path('delete/<int:id>/', DeleteMessage.as_view(), name='message-delete'),
-    path('send-email/', CreateEmailMessage.as_view(), name='email-message'),
+    path('send-email/', sendEmailMSG.as_view(), name='email-message'),
     path('send-bulk-sms/', SendBulkSMS.as_view(), name='send-bulk-sms'),
 ]
