2d9e6c931aee4675f94f43d9ed11cdac587f5c4e
otumuyen-gospel <otumsgosepel@gmail.com>
2026-04-30 11:30:16 +0100

    preparing for production

--- apis/build.sh
+++ apis/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-cd apis
 export DJANGO_SETTINGS_MODULE=apis.settings
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
