af32918ee7d2c978cd006c41359773cf5b6a3efc
otumuyen-gospel <otumsgosepel@gmail.com>
2026-05-11 18:17:09 +0100

    fxing issues with superbase file access

--- apis/apis/production_settings.py
+++ apis/apis/production_settings.py
@@ -46,17 +46,16 @@ AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 # Tell Django to use S3 for media files in production
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_CUSTOM_DOMAIN = f'imlqklfexwpjkjjviiha.supabase.co/storage/v1/object/sign/{AWS_STORAGE_BUCKET_NAME}'
 SERVICE_ROLE_KEY=os.environ.get('SERVICE_ROLE_KEY')
 SUPERBASE_URL=os.environ.get('SUPERBASE_URL')
 
-
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+#AWS_S3_CUSTOM_DOMAIN = f'imlqklfexwpjkjjviiha.supabase.co/storage/v1/object/sign/{AWS_STORAGE_BUCKET_NAME}'
+#MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 AWS_S3_ADDRESSING_STYLE = "path"               # Required for Supabase
 AWS_S3_SIGNATURE_VERSION = "s3v4"              # Supabase requires SigV4
 AWS_QUERYSTRING_AUTH = True
 # Ensure path style is used
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_FORCE_PATH_STYLE = True # Important for Supabase
+#AWS_S3_FILE_OVERWRITE = False
+#AWS_DEFAULT_ACL = None
+#AWS_FORCE_PATH_STYLE = True # Important for Supabase
